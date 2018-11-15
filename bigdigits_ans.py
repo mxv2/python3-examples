7e93abac0af66a52c2977d409be8ac9442947cbd
Maksim Kiryanov <mxvkiryanov@gmail.com>
2018-11-15 19:28:54 +0300

    improve bigidigits.py
    use digit instead *

---
+++ bigdigits_ans.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+_DIGITS = [["  ***  ",
+            " *   * ",
+            "*     *",        
+            "*     *",
+            "*     *",
+            " *   * ",
+            "  ***  "],   
+           ["   *   ",
+            "  **   ",
+            "   *   ",        
+            "   *   ",
+            "   *   ",
+            "   *   ",
+            "  ***  "],
+           ["  ***  ",
+            " *   * ",
+            " *  *  ",        
+            "   *   ",
+            "  *    ",
+            " *     ",
+            " ***** "],
+           ["  ***  ",
+            "     * ",
+            "     * ",        
+            "   **  ",
+            "     * ",
+            "     * ",
+            "  ***  "],
+           ["    *  ",
+            "   **  ",
+            "  * *  ",        
+            " *  *  ",
+            " ******",
+            "    *  ",
+            "    *  "],
+           ["  **** ",
+            "  *    ",
+            "  *    ",        
+            "  ***  ",
+            "     * ",
+            "     * ",
+            "  ***  "],
+           ["   *   ",
+            "  *    ",
+            " *     ",        
+            " ****  ",
+            " *   * ",
+            " *   * ",
+            "  ***  "],
+           [" ***** ",
+            "     * ",
+            "    *  ",        
+            "   *   ",
+            "  *    ",
+            " *     ",
+            " *     "],
+           ["  ***  ",
+            " *   * ",
+            " *   * ",        
+            "  ***  ",
+            " *   * ",
+            " *   * ",
+            "  ***  "],
+           ["  ***  ",
+            " *   * ",
+            " *   * ",        
+            "  **** ",
+            "     * ",
+            "    *  ",
+            "   *   "]]
+
+_MAX_DIGIT_LINES = 7
+
+try:
+    number = sys.argv[1]
+    line_index = 0
+    while line_index < _MAX_DIGIT_LINES:
+        line = ""
+        number_index = 0
+        while number_index < len(number):
+            digit = int(number[number_index])
+            for sym in _DIGITS[digit][line_index]:
+                if sym == "*":
+                    line += str(digit)
+                else:
+                    line += sym
+            line += " "
+            number_index += 1
+        print(line)
+        line_index += 1
+except IndexError:
+    print("usage bigdigits.py <number>")
+except ValueError as err:
+   print(err)
+ 
