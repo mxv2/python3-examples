da1f1337c2f5347c95f54750e4c849e37343355e
Maksim Kiryanov <mxvkiryanov@gmail.com>
2019-01-03 17:12:13 +0300

    add uniquewords2.py
    extend uniquewords1 by defaultdict

---
+++ ch03_collections/uniquewords2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import collections
+import string
+import sys
+
+words = collections.defaultdict(int)
+strip = string.whitespace + string.punctuation + string.digits + "\"'"
+for filename in sys.argv[1:]:
+    for line in open(filename):
+        for word in line.lower().split():
+            word = word.strip()
+            if len(word) > 2:
+                words[word] += 1
+for word in sorted(words):
+    print("{0} occurs {1} times".format(word, words[word]))
