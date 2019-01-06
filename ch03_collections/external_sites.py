8272197dd2e4248de3f909e2f44a0dd117caab66
Maksim Kiryanov <mxvkiryanov@gmail.com>
2019-01-06 15:24:48 +0300

    add external_sites.py
    program collects web-site urls from files

---
+++ ch03_collections/external_sites.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+
+sites = {}
+for filename in sys.argv[1:]:
+    for line in open(filename):
+        i = 0
+        while True:
+            site = None
+            i = line.find("http://", i)
+            if i > -1:
+                i += len("http://")
+                j = 0
+                for j in range(i, len(line)):
+                    if not (line[j].isalnum() or line[j] in ".-"):
+                        site = line[i:j].lower()
+                        break
+                if site and "." in site:
+                    sites.setdefault(site, set()).add(filename)
+                i = j
+            else:
+                break
+
+print(sites)
