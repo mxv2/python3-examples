5ab041a9a8d675f8ee5a749eef5d72a328d0f497
Maksim Kiryanov <mxvkiryanov@gmail.com>
2019-03-08 00:32:58 +0300

    refactor gathering fs stats

--- ch05_modules/ls.py
+++ ch05_modules/ls.py
@@ -1,60 +1,73 @@
 #!/usr/bin/env python3
 import os
+import sys
+
+FULL_PATH, IS_FILE, FILE_SIZE, FILE_MTIME = range(4)
 
 
 def is_hidden_path(path):
     return path.startswith(".")
 
 
-def by_path_name(stat_entry):
-    return stat_entry[0]
+def by_path_name(stat_item):
+    return stat_item[0]
 
+
+def gather_fs_stats_simply(root, show_hidden):
+    stats = []
+    for filename in os.listdir(root):
+        if not show_hidden and is_hidden_path(filename):
+            continue
+        full_path = os.path.join(root, filename)
+        if not os.path.isdir(full_path):
+            stat = os.stat(full_path, follow_symlinks=False)
+            data = (full_path, True, stat.st_size, stat.st_mtime_ns)
+        else:
+            data = (full_path, False, None, None)
+        stats.append(data)
+    return stats
+
+
+def gather_fs_stats_recursively(root_name, show_hidden):
+    stats = []
+    for root, dirnames, filenames in os.walk(root_name):
+        for path in dirnames:
+            if not show_hidden and is_hidden_path(path):
+                continue
+            full_path = os.path.join(root, path)
+            stats.append((full_path, False, None, None))
+        for path in filenames:
+            if not show_hidden and is_hidden_path(path):
+                continue
+            full_path = os.path.join(root, path)
+            stat = os.stat(full_path, follow_symlinks=False)
+            stats.append((full_path, True, stat.st_size, stat.st_mtime_ns))
+    return stats
+
+
+def gather_fs_stats(root_names, recursive, show_hidden):
+    stats = []
+    if not recursive:
+        for root_name in root_names:
+            stats.extend(gather_fs_stats_simply(root_name, show_hidden))
+    else:
+        for root_name in root_names:
+            stats.extend(gather_fs_stats_recursively(root_name, show_hidden))
+    return stats
 
 def main():
-    # TODO: sanitize input roots - expanduser, expandvars, abspath
-    root_names = ["/home/mkiryanov/", "~/.local/"]
-    expanded_roots_iter = zip(root_names,
-                              [os.path.abspath(os.path.expanduser(os.path.expandvars(r))) for r in root_names])
+    root_names = sys.argv[1:]
 
     recursive = False
-    show_hidden = False
+    show_hidden = True
 
-    stats = {}
-    if not recursive:
-        for root_name, root_abs_path in expanded_roots_iter:
-            for path in os.listdir(root_abs_path):
-                if not show_hidden and is_hidden_path(path):
-                    continue
-                file_abs_path = os.path.join(root_abs_path, path)
-                path_name = os.path.join(root_name, path)
-                if not os.path.isdir(file_abs_path):
-                    stat = os.stat(file_abs_path)
-                    data = (path_name, True, stat.st_size, stat.st_mtime_ns)
-                else:
-                    data = (path_name, False, None, None)
-                stats.update({file_abs_path: data})
-    else:
-        for root_name, root_abs_path in expanded_roots_iter:
-            for root, dirnames, filenames in os.walk(root_abs_path):
-                for path in dirnames:
-                    if not show_hidden and is_hidden_path(path):
-                        continue
-                    path_name = os.path.join(root, path)
-                    stats.update({path: (path_name, False, None, None)})
-                for path in filenames:
-                    if not show_hidden and is_hidden_path(path):
-                        continue
-                    file_abs_path = os.path.join(root, path)
-                    stat = os.stat(file_abs_path)
-                    path_name = os.path.join(root, path)
-                    data = (path_name, True, stat.st_size, stat.st_mtime_ns)
-                    stats.update({file_abs_path: data})
+    stats = gather_fs_stats(root_names, recursive, show_hidden)
 
-    for path_name, stat in sorted(stats.items(), key=by_path_name):
-        if stat[1]:
-            print(stat[0], stat[2], stat[3])
+    for stat in sorted(stats, key=by_path_name):
+        if stat[IS_FILE]:
+            print(stat[FULL_PATH], stat[FILE_SIZE], stat[FILE_MTIME])
         else:
-            print(stat[0])
+            print(stat[FULL_PATH])
 
 
 main()
