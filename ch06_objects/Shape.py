7c3842ca07a2f1d9bf5fe04e45fdb22269764df0
Maksim Kiryanov <m.kiryanov@hh.ru>
2019-03-14 18:48:06 +0300

    add Circle class to Shape module

--- ch06_objects/Shape.py
+++ ch06_objects/Shape.py
@@ -17,3 +17,55 @@ class Point:
 
     def __str__(self):
         return "({0.x!r}, {0.y!r})".format(self)
+
+
+class Circle(Point):
+    def __init__(self, radius, x=0, y=0):
+        super().__init__(x, y)
+        self.radius = radius
+
+    @property
+    def radius(self):
+        """The circle's radius
+
+        >>> circle = Circle(-2)
+        Traceback (most recent call last):
+        ...
+        AssertionError: radius must be nonzero and non-negative
+        >>> circle = Circle(4)
+        >>> circle.radius = -1
+        Traceback (most recent call last):
+        ...
+        AssertionError: radius must be nonzero and non-negative
+        >>> circle.radius = 6
+        """
+        return self.__radius
+
+    @radius.setter
+    def radius(self, radius):
+        assert radius > 0, "radius must be nonzero and non-negative"
+        self.__radius = radius
+
+    def edge_distance_from_origin(self):
+        return abs(self.distance_from_origin() - self.radius)
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius and super().__eq__(other)
+
+    def __repr__(self):
+        return "Circle({0.radius!r}, {0.x!r}, {0.y!r})".format(self)
+
+    def __str__(self):
+        return repr(self)
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
