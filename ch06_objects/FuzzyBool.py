22444814c00a9b2c9d07c6f8a46d89099745b7c4
Maksim Kiryanov <mxvkiryanov@gmail.com>
2019-03-21 20:10:47 +0300

    implement special functions
    * casting to bool, int, float
    * bitwise operations
    * fix repr function

--- ch06_objects/FuzzyBool.py
+++ ch06_objects/FuzzyBool.py
@@ -19,13 +19,85 @@ class FuzzyBool:
         >>> repr(f)
         'FuzzyBool(0.5)'
         """
-        return "FuzzyBool({0!r})".format(self.__value)
+        return ("{0}({1})".format(self.__class__.__name__,
+                                  self.__value))
+
+    def __bool__(self):
+        """
+        >>> bool(FuzzyBool(.5))
+        False
+        >>> bool(FuzzyBool(.75))
+        True
+        """
+        return self.__value > 0.5
+
+    def __int__(self):
+        """
+        >>> int(FuzzyBool(.5))
+        0
+        >>> int(FuzzyBool(.75))
+        1
+        """
+        return round(self.__value)
+
+    def __float__(self):
+        """
+        >>> float(FuzzyBool(.5))
+        0.5
+        >>> float(FuzzyBool(.75))
+        0.75
+        """
+        return self.__value
 
     def __hash__(self):
         return hash(id(self))
 
     def __format__(self, format_spec):
         return super().__format__(format_spec)
+
+    def __invert__(self):
+        """
+        >>> f = ~FuzzyBool(0.3)
+        >>> repr(f)
+        'FuzzyBool(0.7)'
+        """
+        return FuzzyBool(1.0 - self.__value)
+
+    def __and__(self, other):
+        """
+        >>> f = FuzzyBool(0.3) & FuzzyBool(0.7)
+        >>> repr(f)
+        'FuzzyBool(0.3)'
+        """
+        return FuzzyBool(min(self.__value, other.__value))
+
+    def __iand__(self, other):
+        """
+        >>> f = FuzzyBool(0.7)
+        >>> f &= FuzzyBool(0.3)
+        >>> repr(f)
+        'FuzzyBool(0.3)'
+        """
+        self.__value = min(self.__value, other.__value)
+        return self
+
+    def __or__(self, other):
+        """
+        >>> f = FuzzyBool(0.3) | FuzzyBool(0.7)
+        >>> repr(f)
+        'FuzzyBool(0.7)'
+        """
+        return FuzzyBool(max(self.__value, other.__value))
+
+    def __ior__(self, other):
+        """
+        >>> f = FuzzyBool(0.3)
+        >>> f |= FuzzyBool(0.7)
+        >>> repr(f)
+        'FuzzyBool(0.7)'
+        """
+        self.__value = max(self.__value, other.__value)
+        return self
 
 
 if __name__ == '__main__':
